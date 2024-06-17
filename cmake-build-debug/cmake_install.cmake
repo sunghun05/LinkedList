c411beda6d044a6a62bf8ad77cbbad51ce930ed2
sunghun05 <sunghun051107@gmail.com>
2024-06-17 14:48:28 +0900

    update;

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/king/c/linkedList
+# Install script for directory: /Users/king/programming/c/linkedList
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/king/c/linkedList/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/king/programming/c/linkedList/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -4,22 +4,51 @@
 typedef struct{
     int data;     //102
     struct Node *next;
+
 }Node;
 Node *head = NULL;              //head는 첫번째 노드의 주소
 Node *tail = NULL;              //tail은 마지막 노드의 주소
-Node create() {
-    int input = 0;
-    if (head == NULL){          //head가 비어있을 때
-        Node *firstNode = (Node*)malloc(sizeof(Node));
-        head->next = &firstNode;//처음 노드의 주소를 참조
-        tail->next = &firstNode;//처음 노드의 주소를 참조
-        firstNode->data = input;//처음 노드의 데이터 삽입
-    }else {                     //첫번쨰 노드가 이미 존재할 때
-        Node *newNode = (Node*)malloc(sizeof(Node));
-        tail->next = &newNode;  //새로운 노드의 주소를 tail이 참조
-    }
-}
 
+Node create() {
+    Node a;
+    Node b;
+    Node c;
+    a.data = 1;
+    b.data = 2;
+    c.data = 3;
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+    head->next = &a;
+    tail->next = &c;
+}
+void search() {
+    while(head->next) {
+        int **h = head->next;
+        printf("%d", h);
+        head->next = head->next;
+    }
+//    printf("%d", head->data);
+
+}
 void main(void) {
-    create();
+    int menu = 0;
+    do {
+        if(!scanf("%d", &menu)){
+            printf("incorrect number");
+            while(getchar() != '\n');
+            continue;
+        }
+        if(menu == 1){
+            create();
+            while(getchar() != '\n');
+            continue;
+        }
+        if(menu == 2){
+            search();
+            while(getchar() != '\n');
+            continue;
+        }
+        else{ break;}
+    }while(1);
 }
