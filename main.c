b1a65eae339bcc8a33d36915027926bca7f0c4e9
sunghun05 <sunghun051107@gmail.com>
2024-06-17 22:52:50 +0900

    linked_list_create_search;

--- main.c
+++ main.c
@@ -1,54 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct{
-    int data;     //102
+typedef struct Node Node;
+struct Node{
+    int data;
     struct Node *next;
+};
 
-}Node;
-Node *head = NULL;              //head는 첫번째 노드의 주소
-Node *tail = NULL;              //tail은 마지막 노드의 주소
+Node create(Node *head, Node *p, int a) {
+    printf("\n=========================new node create====================\n");
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    printf("\n\nnew node is %d\n", newNode);
 
-Node create() {
-    Node a;
-    Node b;
-    Node c;
-    a.data = 1;
-    b.data = 2;
-    c.data = 3;
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
-    head->next = &a;
-    tail->next = &c;
-}
-void search() {
-    while(head->next) {
-        int **h = head->next;
-        printf("%d", h);
-        head->next = head->next;
+    newNode->data = a;
+    newNode->next = NULL;
+
+    if (head->next == NULL) {       //노드가 아예 없을때
+
+        //printf("\nhead is NULL");
+
+        head->next = (struct Node *) newNode;
+
+    } else {                        //노드가 하나라도 있을때
+        //printf("\nhead is NOT NULL");
+
+        p = head;       //순회하는 포인터가 첫번째 노드를 가리킴
+
+        while(p->next != NULL){
+            p = p->next;
+        }
+        p->next = newNode;
     }
-//    printf("%d", head->data);
+
+//    printf("\n%s", "newNode의 data : "); printf("%x", &newNode->data);
+//    printf("\n%s", "head의 next : "); printf("%x", head->next);
+//
+//    printf("\n\n%d", head->next->data);
+
+}
+void search(Node *head, Node *p) {
+
+    p = head;       //순회하는 포인터가 첫번째 노드를 가리킴
+
+    while(p != NULL){
+        printf("\n%d\n", p->data);
+        p = p->next;
+    }
 
 }
 void main(void) {
-    int menu = 0;
-    do {
-        if(!scanf("%d", &menu)){
-            printf("incorrect number");
-            while(getchar() != '\n');
-            continue;
-        }
-        if(menu == 1){
-            create();
-            while(getchar() != '\n');
-            continue;
-        }
-        if(menu == 2){
-            search();
-            while(getchar() != '\n');
-            continue;
-        }
-        else{ break;}
-    }while(1);
+    Node *head = (Node *)malloc(sizeof(Node));
+    Node *tail = (Node *)malloc(sizeof(Node));
+    Node *p = (Node *)malloc(sizeof(Node));
+    head->data = 0;
+    tail->data = 0;
+    tail->next = NULL;
+    head->next = NULL;
+
+//    printf("%x\n", &head->data);
+//    printf("%x", &head->next);
+//    printf("\n%x", head);
+
+    create(head, p, 0);
+    create(head, p, 1);
+    create(head, p, 2);
+    create(head, p, 3);
+    search(head, p);
 }
